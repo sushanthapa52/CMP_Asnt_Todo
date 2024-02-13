ef9d6d185a92f3018b61fdf82d56700d30679bdf
Dell <sushan.thapa1010@gmail.com>
2024-02-13 09:23:54 -0500

    added todo controller

---
+++ TodoModels/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoModels
+{
+        public class TodoItem
+
+        {
+            public int Id { get; set; }
+            public DateTime DueDate { get; set; }
+            public DateTime? CompletedDate { get; set; } // Nullable DateTime for completion date
+            public string Description { get; set; }
+        }
+    
+}
